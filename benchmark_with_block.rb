f88f34beb1b3e029176ad30cd8feed981cd2c963
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-12 20:03:43 -0700

    Implement benchmark logic

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,9 @@
 def benchmark
   # Your benchmarking code goes here.
+  start_time = Time.now
+  some_task = yield
+  end_time = Time.now
+  return end_time - start_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
